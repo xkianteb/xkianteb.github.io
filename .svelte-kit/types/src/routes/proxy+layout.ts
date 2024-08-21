437730e59614ed3debd1188d132f998cface857b
Kiante Brantley <kiantebrantley@Kiantes-MacBook-Air.local>
2024-08-21 05:06:00 -0400

    ()

--- .svelte-kit/types/src/routes/proxy+layout.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import type { LayoutLoad } from "./$types";
-
-export const prerender = true;
-
-export const load = async ({ url }: Parameters<LayoutLoad>[0]) => {
-  return { pathname: url.pathname };
-};
