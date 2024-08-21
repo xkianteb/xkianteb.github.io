2950b27d0750b0c4fdddabf4aefa418d2bcc0d31
Kiante Brantley <kiantebrantley@Kiantes-MacBook-Air.local>
2024-08-21 12:34:35 -0400

    ()

---
+++ src/lib/components/Seo.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let title: string;
+  export let ogTitle: string | null = null;
+  export let description: string;
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content={description} />
+  <meta property="og:title" content={ogTitle ?? title} />
+  <meta property="og:description" content={description} />
+  <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
