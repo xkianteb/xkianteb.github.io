550307d71cc4a77a167af380028485f951f02e08
Kiante Brantley <kiantebrantley@Kiantes-MacBook-Air.local>
2024-08-21 02:36:36 -0400

    ()

---
+++ build/_app/immutable/chunks/3.6a50a297.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/publications-page.svelte.b785b534.js";export{t as component};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { ArrowUpRight } from "lucide-svelte";
+
+  import Seo from "$lib/components/Seo.svelte";
+</script>
+
+<Seo
+  title="Kiant&eacute; Brantley"
+  description=""
+/>
