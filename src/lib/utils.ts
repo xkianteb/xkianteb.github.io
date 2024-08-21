b784cb75dc076e2505e08a7d4e6865b931646575
Kiante Brantley <kiantebrantley@Kiantes-MacBook-Air.local>
2024-08-21 01:36:31 -0400

    ()

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import strftime from "strftime";
-
-const utc = strftime.utc(); // YAML dates are in UTC
-
-export function formatTime(format: string, date: Date | string): string {
-  return utc(format, new Date(date));
-}
