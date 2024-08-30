cb5856b751d9a4e25b4cdfd3890614383dc04868
Kiante Brantley <kiantebrantley@Kiantes-MacBook-Air.local>
2024-08-30 00:17:09 -0400

    created seperate pubs and talk endpoints

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,10 @@
 import type { PageLoad } from './$types'
 
 export const load: PageLoad = async ({ fetch }) => {
-  const res_bibfile = await fetch(`parse-bibtex?file=bibfile`)
+  const res_bibfile = await fetch(`parse-bibtex-pubs?file=bibfile`)
   const bibtex = await res_bibfile.text()
 
-  const res_talks = await fetch(`parse-bibtex?file=talks`)
+  const res_talks = await fetch(`parse-bibtex-talks?file=talks`)
   const talks = await res_talks.text()
   return { bibtex, talks }
 }
