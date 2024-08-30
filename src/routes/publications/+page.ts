91081f6d8318e5b8bb4c3c40d58aead7ae55ffd5
Kiante Brantley <kiantebrantley@Kiantes-MacBook-Air.local>
2024-08-29 22:23:34 -0400

    ()

--- src/routes/publications/+page.ts
+++ src/routes/publications/+page.ts
@@ -1,10 +1,11 @@
 import type { PageLoad } from './$types'
 
 export const load: PageLoad = async ({ fetch }) => {
-  const res_bibfile = await fetch(`parse-bibtex?file=bibfile`)
+  const res_bibfile = await fetch(`parse-bibtex-pubs?file=bibfile`)
   const bibtex = await res_bibfile.text()
 
-  const res_talks = await fetch(`parse-bibtex?file=talks`)
-  const talks = await res_talks.text()
-  return { bibtex, talks }
+  //const res_talks = await fetch(`parse-bibtex?file=talks`)
+  //const talks = await res_talks.text()
+  //return { bibtex, talks }
+  return {bibtex}
 }
