243c9d08d4033c96ec0ff3a06a85a0d0921cabd4
Kiante Brantley <kiantebrantley@Kiantes-MacBook-Air.local>
2024-11-11 00:44:49 -0500

    ()

--- src/routes/bio/Bio.svelte
+++ src/routes/bio/Bio.svelte
@@ -4,6 +4,6 @@
 <div class="grid grid-cols-1 gap-y-0 ml-4">
   <div>
     Kianté Brantley is an Assistant Professor in the Kempner Institute and  School of Engineering and Applied Sciences (SEAS) at Harvard University. He completed his Ph.D. in Computer Science at the University of Maryland College Park, advised by Dr. Hal Daumé III. After graduating, he completed his postdoctoral studies at Cornell University, working with Thorsten Joachims.His research focuses on problems at the intersection of machine learning and interactive decision-making, with the goal of improving the decision-making capabilities of foundation models.
-He has received several awards with his colleagues, including spotlight talks at ICLR 2023 and ICLR 2019. He is a recipient of the NSF LSAMP BD Fellowship, ACM SIGHPC Computational and Data Science Fellowship, Microsoft Dissertation Research Grant, Ann G. Wylie Dissertation Fellowship, and NSF CIFellow Postdoctoral Fellowship.In his spare time, he enjoys playing sports; his favorite sport at the moment is powerlifting.
+He has received several awards with his colleagues, including spotlight talks at ICLR 2023 and ICLR 2019. He is a recipient of the NSF LSAMP BD Fellowship, ACM SIGHPC Computational and Data Science Fellowship, Microsoft Dissertation Research Grant, Ann G. Wylie Dissertation Fellowship, and NSF CIFellow Postdoctoral Fellowship. In his spare time, he enjoys playing sports; his favorite sport at the moment is powerlifting.
   </div>
 </div>
