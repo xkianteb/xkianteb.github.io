0b00398b0a4170546216a995de7d8a98ddd3a04e
Kiante Brantley <kiantebrantley@Kiantes-MacBook-Air.local>
2024-11-11 00:55:27 -0500

    ()

--- src/routes/teaching/TeachingList.svelte
+++ src/routes/teaching/TeachingList.svelte
@@ -1,17 +1,16 @@
 <div class="grid gap-y-1">
 
-<div class="grid grid-cols-[15%_15%_34%_34%] gap-y-4 ml-4">
+<div class="grid grid-cols-[15%_10%_40%_45%] gap-y-4 ml-4">
   <div class="text-sm font-bold font-sans">Semester:</div>
   <div class="text-sm font-bold font-sans">Course:</div>
   <div class="text-sm font-bold font-sans">Title:</div>
   <div class="text-sm font-bold font-sans">Topics:</div>
 
-  <!--
-  <div class="text-sm">[Semester] [Year]</div>
-  <div class="text-sm">[Course]</div>
-  <div class="text-sm">[Title]</div>
-  <div class="text-sm">[Topic]</div>
-  -->
+ 
+  <div class="text-sm">Spring 2025</div>
+  <div class="text-sm">CS 182</div>
+  <div class="text-sm">Planning and Learning Methods in AI</div>
+  <div class="text-sm">Search, Planning, RL, ML</div>
 </div>
 
 </div>
