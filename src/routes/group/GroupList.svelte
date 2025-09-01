a85e956fe2effcdd56f27b62b565e510c08de565
Kiante Brantley <kiante.brantley@gmail.com>
2025-08-31 20:49:46 -0400

    fixed website

--- src/routes/group/GroupList.svelte
+++ src/routes/group/GroupList.svelte
@@ -5,6 +5,6 @@
   <ul class="space-y-2 list-none">
     <li><a href="https://kempnerinstitute.harvard.edu/people/our-people/nianli-peng/" class="hover:underline alink"> Nianli Peng </a></li>
     <li><a href="https://adzc.ai/" class="hover:underline alink">Alex Cai</a></li>
-    <li><a href="danieldritter.github.io" class="hover:underline alink">Daniel Ritter</a></li>
+    <li><a href="https://danieldritter.github.io" class="hover:underline alink">Daniel Ritter</a></li>
   </ul>
 </div>
